c50ac534f17e1ddcb53c33771ab9dc1e0ec7461c
gagan chhabra <gsgagan85@gmail.com>
2021-04-28 06:41:44 -0400

    Define a few products, size, price and value in the cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 let carts = document.querySelectorAll('.add-to-cart');
 
+let products = [
+    {
+        name: 'women striped crop top',
+        size: 'UK 12',
+        price: '400',
+        inCart: '0'
+    },
+    {
+        name: 'Mens navy blue polo shirt',
+        size: 'Uk 12',
+        price: '600',
+        inCart: '0'
+    },
+    {
+        name: 'Floral cold-shoulder top',
+        size: 'UK 12/14',
+        price: '500',
+        inCart: '0'
+    },
+    {
+        name: 'Mens striped casual shirt',
+        size: 'UK 10',
+        price: '600',
+        inCart: '0'
+    }
+]
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
         cartNumbers();
