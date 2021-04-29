2df6df0ed3a99f8a64a647c2e01672f59e1d7a2a
Immagitonga25 <immaculate.gitonga@student.moringaschool.com>
2021-04-29 13:01:38 +0300

    add a checkout section in  the products page

--- js/products.js
+++ js/products.js
@@ -1,63 +1,62 @@
 //Business logic
-
- //adding items to checkout
+var item;
+var price;
+var title;
+var image;
+//adding items to checkout
 //  function addItemToCart()
 
-var delivery = document.getElementById("deliveryoption");
-
-//  if (delivery === "delivery") {
-//   var location = prompt("Enter delivery location: ");
-//   alert(
-//     "Your order will be delivered at " +
-//       location +
-//       ". Kindly pay the rider Ksh 200 on arrival."
-//   );
-// } else {
-//   alert("Pick up your order from the Pick-up Point.");
-// }
-
 //User logic
-$(document).ready(function() {
+$(document).ready(function () {
+  //addToCart button
+  var addToCartBtn = document.getElementsByClassName("item-button");
+  for (var i = 0; i < addToCartBtn.length; i++) {
+    var button = addToCartBtn[i];
+    button.addEventListener("click", addToCartClicked);
+  }
+  $("button.item-button").click(function () {
+    alert("Item added to cart!");
 
-   //addToCart button
-   var addToCartBtn = document.getElementsByClassName("item-button")
-   for(var i = 0; i < addToCartBtn.length; i++) {
-       var button = addToCartBtn[i]
-       button.addEventListener("click", addToCartClicked)
-   }
-   $("button.item-button").click(function(){
-       alert("Item added to cart!")
-   });
-   function addToCartClicked(event) {
-    var button = event.target
-    var item = button.parentElement.parentElement
-    var title = item.getElementsByClassName("item-title")[0].innerText
-    var price = item.getElementsByClassName("item-price")[0].innerText
-    var image = item.getElementsByClassName("item-image")[0].src
-    console.log(title, price, image)
+    $("tbody#checkout").append(
+      "<tr><td>" +
+        title +
+        "</td>" +
+        "<td>" +
+        price +
+        "</td>" +
+        "<td>" +
+        quantity +
+        "</td>" +
+        "<td>" +
+        "<button>REMOVE</button>" +
+        "</td></tr>"
+    );
+  });
+  function addToCartClicked(event) {
+    var button = event.target;
+    item = button.parentElement.parentElement;
+    title = item.getElementsByClassName("item-title")[0].innerText;
+    price = item.getElementsByClassName("item-price")[0].innerText;
+    image = item.getElementsByClassName("item-image")[0].src;
+    console.log(title, price, image);
+  }
 
-    var delivery = $("#deliveryoption option:selected").val();
-    console.log(delivery) 
+  var quantity = 1;
+  var total = 
+  $("#purchase").click(function () {
     
-    var quantity = 1
-    $("#purchase").click(function(){
-
-        //delivery prompt
-        
-
-        if (delivery === "delivery") {
-            var location = prompt("Enter delivery location: ");
-            alert(
-              "Your order will be delivered at " +
-                location +
-                ". Kindly pay the rider Ksh 200 on arrival."
-            );
-          } else {
-            alert("Pick up your order from the Pick-up Point.");
-          }
-    })
-}
-
-   
-  
+    //delivery prompt
+    var delivery = $("#deliveryoption option:selected").val();
+    console.log(delivery);
+    if (delivery === "delivery") {
+      var location = prompt("Enter delivery location: ");
+      alert(
+        "Your package will be delivered at " +
+          location +
+          ". Kindly pay the rider Ksh 200 on arrival."
+      );
+    } else {
+      alert("Pick up your order from the Pick-up Point.");
+    }
+  });
 });
